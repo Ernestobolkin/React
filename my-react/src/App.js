c1b0b7b1b993b8b0364ea859085a7bace4ad9549
Ernestobolkin <ernest1047@gmail.com>
2021-12-13 15:14:31 +0200

    task
    task 7.1 is done

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { BtnContainer } from "./components/4.1_buttons/BtnContainer";
-import { Cards } from "./components/4.2_cards/cards";
+// import { BtnContainer } from "./components/4.1_buttons/BtnContainer";
+// import { Cards } from "./components/4.2_cards/cards";
+import { Input_OutPut } from "./components/7.1_increment/increment";
 
 const App = () => {
   return (
     <div>
-      <BtnContainer />
-      <Cards />
+      {/* <BtnContainer />
+      <Cards /> */}
+      <Input_OutPut/>
     </div>
   );
 };
---
+++ my-react/src/components/7.1_increment/increment.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class Input_OutPut extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 0 };
+  }
+  Inc(){
+    this.setState({num:this.state.num+1})
+  }
+  Dec(){
+    this.setState({num:this.state.num-1})
+  }
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        <button onClick={this.Inc.bind(this)} id="btn" style={{ marginRight: "10px" }}>
+          increment
+        </button>
+        <button onClick={this.Dec.bind(this)} id="btn" style={{ marginRight: "10px" }}>
+          decrement
+        </button>
+        <label htmlFor="btn">{this.state.num}</label>
+      </div>
+    );
+  }
+}
