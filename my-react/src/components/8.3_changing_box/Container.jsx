1c6bb996c6ce34f05e641869b2a61e7ac9fa3219
Ernestobolkin <ernest1047@gmail.com>
2021-12-14 13:30:03 +0200

    8.3 is done

---
+++ my-react/src/components/8.3_changing_box/Container.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ChangeBox } from "./box";
+
+export class ContainerCircleBox extends React.Component {
+  state = { radius: "" ,color:"green"};
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ color: "red" });
+    }, 1000);
+    setTimeout(() => {
+      this.setState({ color: "blue" });
+    }, 2000);
+    setTimeout(() => {
+      this.setState({ color: "black" });
+    }, 3000);
+  }
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.setState({ radius: "50px" });
+    }, 5000);
+  }
+  render() {
+    return (
+      <div>
+        <ChangeBox radius={this.state.radius} color={this.state.color} />
+      </div>
+    );
+  }
+}
