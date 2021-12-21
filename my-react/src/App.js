94b6e180721d87c205a42be23ce6b6ffdf072c00
Ernestobolkin <ernest1047@gmail.com>
2021-12-21 23:14:55 +0200

    all done

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { DataContainer } from "./components/13.1_data_massaging/dataContainer";
+import { BrowserRouter, Route } from "react-router-dom";
+import { HomePage } from "./components/16.1_products/homePage/HomePage";
+import { Products } from "./components/16.1_products/products/Products";
+import { NavBar } from "./components/16.1_products/header/NavBar";
+import { ProductDetail } from "./components/16.1_products/products/ProductDetail";
 
 const App = () => {
   return (
-    <div>
-      <DataContainer />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/Products" exact component={Products} />
+      <Route path="/Products/:id/:title" exact component={ProductDetail} />
+    </BrowserRouter>
   );
 };
 
