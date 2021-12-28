bb64d643997d7254bc6d8e33386835cf3bf56e36
Ernestobolkin <ernest1047@gmail.com>
2021-12-28 13:47:20 +0200

    cancel the axios request in useEffect

---
+++ my-react/src/components/20.1_cencel_request/cancel.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export const Cancel = () => {
+  const [isFetch, setIsFecth] = useState(false);
+
+  const Data = () => {
+    const [joke, setJoke] = useState([]);
+
+    useEffect(() => {
+      const cancelToken = axios.CancelToken;
+      const source = cancelToken.source();
+      const getData = async () => {
+        const data = await axios
+          .get("https://api.chucknorris.io/jokes/random", {
+            cancelToken: source.token,
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        setJoke(data);
+      };
+      getData();
+
+      return () => {
+        source.cancel();
+      };
+    }, []);
+    return <div>{joke.value}</div>;
+  };
+
+  return (
+    <div>
+      <button onClick={() => setIsFecth(!isFetch)}>
+        {!isFetch ? "Fetch" : "Enable"}
+      </button>
+      {isFetch && <Data />}
+    </div>
+  );
+};
