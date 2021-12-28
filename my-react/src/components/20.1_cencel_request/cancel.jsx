0431a8e991438e8b83585a0308ccf95fae4259e5
Ernestobolkin <ernest1047@gmail.com>
2021-12-28 15:45:20 +0200

    🐛

--- my-react/src/components/20.1_cencel_request/cancel.jsx
+++ my-react/src/components/20.1_cencel_request/cancel.jsx
@@ -3,13 +3,12 @@ import axios from "axios";
 
 export const Cancel = () => {
   const [isFetch, setIsFecth] = useState(false);
-
   const Data = () => {
     const [joke, setJoke] = useState([]);
 
     useEffect(() => {
-      const cancelToken = axios.CancelToken;
-      const source = cancelToken.source();
+      // const cancelToken = axios.CancelToken;
+      const source = axios.CancelToken.source();
       const getData = async () => {
         const data = await axios
           .get("https://api.chucknorris.io/jokes/random", {
